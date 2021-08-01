636ccc8640418959ce087a0d47a29ecf54cd7d71
Lukas Bagaric <deaeod@gmail.com>
2021-08-01 18:47:03 +0200

    Dump .int During Build

--- build.bat
+++ build.bat
@@ -27,11 +27,14 @@ if ERRORLEVEL 1 goto cleanup
 
 :: Generate compressed file for redirects
 ucc compress %PACKAGE_NAME%.u
+:: dump localization strings
+ucc dumpint %PACKAGE_NAME%.u
 
 :: copy to release location
 if not exist %BUILD_DIR%System (mkdir %BUILD_DIR%System)
 copy %PACKAGE_NAME%.u     %BUILD_DIR%System >NUL
 copy %PACKAGE_NAME%.u.uz  %BUILD_DIR%System >NUL
+copy %PACKAGE_NAME%.int   %BUILD_DIR%System >NUL
 
 popd
 
